73444ce0feddf2a3018d78e3a1d0ac49401cc8de
MadsKK <35922365+MadsKK@users.noreply.github.com>
2019-11-18 13:06:50 +0100

    Update sorteringsAlgoritmer.py

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,18 +1,15 @@
 
 def insertionSort(liste):
-    sortedList = []
-    indexList = []
-    for x in range(len(liste)):
-        print("x1: " + str(x))
-        indexList += [x]
-        for n in range(indexList[-1]):
-            print("x2: " + str(x))
-            print("n: " + str(n))
-            if x < n:
 
-                sortedList += [liste[x]]
+    for i in range(1, len(liste)):
+        key = liste[i]
+        j = i-1
+        while j >= 0 and key < liste[j]:
+            liste[j+1] = liste[j]
+            j -= 1
+        liste[j+1] = key
 
-    return sortedList
+    return liste
 
 
 def selectionSort():
@@ -29,4 +26,5 @@ def mergeSort():
 
 if __name__ == "__main__":
 
-    print(insertionSort(['agftd', 'ckfdfo', 'bkeoksess']))
+    print(insertionSort(['agftd', 'ckfdfo', 'bkeoksess',
+                         'fsdfsdofksdofds', 'lokesomses', 'zsfkdo', 'pfe']))
