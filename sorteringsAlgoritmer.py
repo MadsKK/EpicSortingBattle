54fd641ea0802ea157f51e00da89b4ddf382eda6
MadsKK <35922365+MadsKK@users.noreply.github.com>
2019-11-21 10:58:19 +0100

    Update sorteringsAlgoritmer.py

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,15 +1,18 @@
-def insertionSort(liste):
+def insertionSort(liste1):
+
+    liste = liste1.copy()
+
     # Første for loop som holder styr på alle elementer
     for i in range(1, len(liste)):
         key = liste[i]
-        # Gemmer det forrige element
+        # Gemmer det forrige elements position
         j = i-1
         # Hvis vi ikke er i starten, altså første element, og det nuværende
         # element er mindre end det forrige element
         while j >= 0 and key < liste[j]:
             # Sæt det nuværende element til det forrige element
             liste[j+1] = liste[j]
-            # Tæller ned, så vi til sidst rammer starten. 8..7..6..5..4..3..2..1
+            # Tæller ned så vi til sidst rammer starten. 8..7..6..5..4..3..2..1
             j -= 1
         liste[j+1] = key
 
@@ -20,7 +23,10 @@ def selectionSort():
     pass
 
 
-def bubbleSort(liste):
+def bubbleSort(liste1):
+
+    liste = liste1.copy()
+
     # Første for loop som holder styr på alle elementerne
     for i in range(1, len(liste)):
         # Anden for loop som laver en bubble omkring 2 elementer og tjekker dem
@@ -41,7 +47,8 @@ def mergeSort():
 
 if __name__ == "__main__":
 
-    print(insertionSort(['agftd', 'ckfdfo', 'bkeoksess',
-                         'fsdfsdofksdofds', 'lokesomses', 'zsfkdo', 'pfe']))
-    # print(selectionSort(['agftd', 'ckfdfo', 'bkeoksess',
-    #                    'fsdfsdofksdofds', 'lokesomses', 'zsfkdo', 'pfe']))
+    test = ['agftd', 'ckfdfo', 'bkeoksess',
+            'fsdfsdofksdofds', 'lokesomses', 'zsfkdo', 'pfe']
+    # print(insertionSort(test))
+    # print(selectionSort(test))
+    print(bubbleSort(test))
