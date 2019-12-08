052d01a679e1c16a659b3c9d17c68ed2e004d0be
MadsKK <35922365+MadsKK@users.noreply.github.com>
2019-12-08 18:41:27 +0100

    Bigger list

--- unittesting.py
+++ unittesting.py
@@ -5,7 +5,8 @@ import sorteringsAlgoritmer
 class TestSortAlgorithm(unittest.TestCase):
 
     def setUp(self):
-        self.testList = [9, 4, 6, 2, 5]
+        self.testList = [9, 4, 6, 2, 5, 4, 8, 7, 3, 4, 9, 1, 5, 4, 2, 6, 7, 8, 6,
+                         4, 8, 1, 2, 6, 7, 3, 489, 4, 1, 568, 4, 1, 8, 4, 1, 348, 435156, 468]
 
     def testInsertionSort(self):
         self.sortedList = sorteringsAlgoritmer.insertionSort(self.testList)
